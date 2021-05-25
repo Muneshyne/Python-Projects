b7b39ff91a3f0f510fa532eea8fca9af268889e0
Jeremy Munshower <jdmunshower@gmail.com>
2021-05-24 23:22:02 -0700

    assignment

---
+++ tutorials/Parentchildassignment.py
@@ -0,0 +1,17 @@
+
+
+class User:
+      name = "No Name Provided"
+      email = " "
+      password = "password123"
+      reciept_number = 10
+
+
+class Customer(User):
+      area = "Retail", "Diner"
+      phone_reward = " "
+
+class Employee(User):
+      address = " "
+      pay_rate = 15.00
+      
---
+++ tutorials/createing_class.py
@@ -0,0 +1,21 @@
+
+
+class User:
+      #Define the attributes of the class
+      name = "No Name Provided"
+      email = ""
+      password = "1234abcd"
+      account = 0
+
+      #Define the metods of the class
+      def login(self):
+            entry_email = input("Enter your email: ")
+            entry_password = input("Enter your password: ")
+            if (entry_email == self.email and entry_password == self.password):
+                  print("Welcome back, {}".format(self.name))
+            else:
+                  print("You are not authorized for this page.")
+#Outside of the class you would create an instance of the User class
+new_user = User()
+#Call the login method using the new object
+new_user.login()
