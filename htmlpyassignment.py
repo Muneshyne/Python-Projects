1b1096b424e7e04ee7f210a553086a94adb34bb5
Jeremy Munshower <jdmunshower@gmail.com>
2021-06-01 18:15:53 -0700

    edit pya

--- htmlpyassignment.py
+++ htmlpyassignment.py
@@ -3,33 +3,30 @@ import webbrowser
 import os
 from tkinter import *
 
-f = open("webgen.html", "w")
 
-html_template = """
-<html>
+def Submit():
+    text = txtfld.get()
+    html_template = """
+    <html>
+    <body>
+    <h1>
+    {}
+    </h1>
+    </body>
+    </html>
+    """.format(text)
+    f = open("webgen.html", "w")
+    #writing code into file
+    f.write(html_template)
+    f.close()
 
-<body>
-
-<h1>
-
-Stay tuned for our amazing summer sale!
-
-</h1>
-
-</body>
-
-</html>
-"""
-#writing code into file
-f.write(html_template)
-f.close()
-
-filename = 'file:///'+os.getcwd()+'/' + 'webgen.html'
-webbrowser.open_new_tab(filename)
-
+    filename = 'file:///'+os.getcwd()+'/' + 'webgen.html'
+    webbrowser.open_new_tab(filename)
 window=Tk()
-btn=Button(window, text="To confirm body click here", fg='blue')
+btn=Button(window, text="To confirm body click here", fg='blue', command=Submit)
 btn.place(x=80, y=100)
+lbl=Label(window, text="Please Enter Header", fg='red', font=("Helvetica", 16))
+lbl.place(x=60, y=50)
 txtfld=Entry(window, text="", bd=5)
 txtfld.place(x=80, y=150)
 window.title('Web Generator')
