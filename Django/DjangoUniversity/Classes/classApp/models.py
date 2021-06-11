b5f3b9822be5ecdf8a207b5b6cc720a054ca6cd3
Jeremy Munshower <jdmunshower@gmail.com>
2021-06-11 14:18:52 -0700

    .

--- Django/DjangoUniversity/Classes/classApp/models.py
+++ Django/DjangoUniversity/Classes/classApp/models.py
@@ -16,11 +16,11 @@ course = (
 class DjangoClasses(models.Model):
     pass
     title = models.CharField(max_length=60, default="", choices=Classes_AVAIL)
-    courseNumber = models.IntegerField(default=0, choices=course)
+    courseNumber = models.IntegerField(default=0, choices=course)  # default int 0 no ""
     instructorName = models.CharField(max_length=60, default="")
     classDuration = models.FloatField(default=1.0)
-
+    # needed defaults otherwise errors
     objects = models.Manager()
 
     def __str__(self):
-        return self.instructorName
+        return self.instructorName  # shows the name of the instructor in admin page
